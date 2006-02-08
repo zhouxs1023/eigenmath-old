194856e1d9ac79b8520951797c4e5b85bce3db52
George Weigt <gweigt@users.sourceforge.net>
2006-02-08 13:59:41 -0700

    edit

--- 217.c
+++ 217.c
@@ -12,16 +12,66 @@ unsigned char *buf = "The rain in Spain.";
 
 main()
 {
+	method0();
 	method1();
 	method2();
 }
 
-// method 1 uses bit shifting
+// this method uses bit shifting
+
+method0()
+{
+	unsigned int i, j, r;
+
+	/* first four bytes of packet */
+
+	r = (unsigned int) buf[0]
+	  | (unsigned int) buf[1] << 8
+	  | (unsigned int) buf[2] << 16
+	  | (unsigned int) buf[3] << 24;
+
+	r = ~r; /* invert first 32 bits */
+
+	for (i = 4; i < 18; i++) {
+
+		/* shift 8 bits */
+
+		for (j = 0; j < 8; j++)
+			if (r & 1)
+				r = (r >> 1) ^ G;
+			else
+				r >>= 1;
+
+		/* fix up last 8 bits */
+
+		r ^= (unsigned int) buf[i] << 24;
+	}
+
+	/* shift in 32 zero bits */
+
+	for (i = 0; i < 32; i++)
+		if (r & 1)
+			r = (r >> 1) ^ G;
+		else
+			r >>= 1;
+
+//	r = ~r; /* invert result */
+
+//	printf("FCS = %02x %02x %02x %02x\n",
+//		r & 0xff, /* 1st byte sent */
+//		r >> 8 & 0xff,
+//		r >> 16 & 0xff,
+//		r >> 24);
+
+	printf("%#x\n", r);
+}
+
+// this method also uses bit shifting
 
 method1()
 {
 	int i, j;
-	unsigned int crc = 0;
+	unsigned int crc = ~0;
 	for (i = 0; i < 18; i++) {
 		crc ^= buf[i];
 		for (j = 0; j < 8; j++)
@@ -40,7 +90,7 @@ unsigned int shift16[65536];
 method2()
 {
 	int i;
-	unsigned int crc = 0;
+	unsigned int crc = ~0;
 	make_table();
 	for (i = 0; i < 18; i += 2) {
 		crc ^= (buf[i + 1] << 8) | buf[i];
