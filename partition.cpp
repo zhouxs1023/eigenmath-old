a8576ca71d694dc939f590d5a15272f6740c1bb5
George Weigt <gweigt@users.sourceforge.net>
2006-06-02 09:03:42 -0700

    edit

--- partition.cpp
+++ partition.cpp
@@ -24,43 +24,30 @@ partition(void)
 	p2 = pop();
 	p1 = pop();
 
-	p3 = symbol(NIL);
-	p4 = symbol(NIL);
+	push_integer(1);
+
+	p3 = pop();
+	p4 = p3;
 
 	p1 = cdr(p1);
 
 	while (iscons(p1)) {
 		if (find(car(p1), p2)) {
-			if (p4 == symbol(NIL))
-				p4 = car(p1);
-			else {
-				push(p4);
-				push(car(p1));
-				multiply();
-				p4 = pop();
-			}
+			push(p4);
+			push(car(p1));
+			multiply();
+			p4 = pop();
 		} else {
-			if (p3 == symbol(NIL))
-				p3 = car(p1);
-			else {
-				push(p3);
-				push(car(p1));
-				multiply();
-				p3 = pop();
-			}
+			push(p3);
+			push(car(p1));
+			multiply();
+			p3 = pop();
 		}
 		p1 = cdr(p1);
 	}
 
-	if (p3 == symbol(NIL))
-		push_integer(1);
-	else
-		push(p3);
-
-	if (p4 == symbol(NIL))
-		push_integer(1);
-	else
-		push(p4);
+	push(p3);
+	push(p4);
 
 	restore();
 }
