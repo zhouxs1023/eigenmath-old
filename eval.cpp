05d37dbceffd12301fdb6e08837f1387b7cc93d9
George Weigt <gweigt@users.sourceforge.net>
2006-10-05 16:47:54 -0700

    *** empty log message ***

--- eval.cpp
+++ eval.cpp
@@ -317,39 +317,24 @@ eval_dsolve(void)
 void
 eval_eval(void)
 {
-	int h = tos;
-
 	push(cadr(p1));
 	eval();
-	p5 = pop();
-
-	p2 = cddr(p1);
-	while (iscons(p2)) {
-		push(car(p2));
+	p1 = cddr(p1);
+	while (iscons(p1)) {
+		push(car(p1));
 		eval();
-		push(cadr(p2));
+		push(cadr(p1));
 		eval();
-		p4 = pop();
+		p1 = cddr(p1);
 		p3 = pop();
-		if (p3->k == SYM) {
-			push(p3);
-			push(p3->u.sym.binding);
-			p3->u.sym.binding = p4;
-		}
-		p2 = cddr(p2);
+		p2 = pop();
+		if (p2 == symbol(NIL) || p3 == symbol(NIL))
+			continue;
+		push(p2);
+		push(p3);
+		subst();
 	}
-
-	push(p5);
 	eval();
-	p5 = pop();
-
-	while (tos > h) {
-		p4 = pop();
-		p3 = pop();
-		p3->u.sym.binding = p4;
-	}
-
-	push(p5);
 }
 
 void
